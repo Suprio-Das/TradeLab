0459e6b1862bdb2add4422621f7693f632be9ae2
Suprio Das <suprio.cse@gmail.com>
2026-04-19 11:29:20 +0600

    Navbar and Footer created and added to the MainLayout

--- frontend/src/Layout/MainLayout.jsx
+++ frontend/src/Layout/MainLayout.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Navbar from '../Components/Navbar';
+import Footer from '../Components/Footer';
 
 const MainLayout = () => {
     return (
         <div>
-            <h1>Main Layout</h1>
+            <Navbar></Navbar>
+            <Footer></Footer>
         </div>
     );
 };
