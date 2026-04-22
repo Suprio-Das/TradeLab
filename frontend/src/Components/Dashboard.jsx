884fae2096133755312d6cffb0277a5327c6da4c
Suprio Das <suprio.cse@gmail.com>
2026-04-22 22:46:15 +0600

    Dashboard access denied without login

--- frontend/src/Components/Dashboard.jsx
+++ frontend/src/Components/Dashboard.jsx
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import AuthContext from '../Context/AuthContext';
+import { useNavigate } from 'react-router-dom';
 
 const Dashboard = () => {
+    const { user, loading } = useContext(AuthContext);
+    const navigate = useNavigate();
+    if (loading) {
+        return (
+            <div className="flex items-center justify-center min-h-screen">
+                <p className="text-xl font-semibold">
+                    Moneys are printing...
+                </p>
+            </div>
+        );
+    }
+    if (!user) {
+        navigate('login');
+    }
     return (
         <div>
             <h1>Dashboard.</h1>
