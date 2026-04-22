844410ce2bb3fe2a1378177a03b5ea8a3d28fd92
Suprio Das <suprio.cse@gmail.com>
2026-04-22 12:10:32 +0600

    Auth observer set

--- frontend/src/Context/AuthProvider.jsx
+++ frontend/src/Context/AuthProvider.jsx
@@ -1,9 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import AuthContext from './AuthContext';
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from '../../firebase.init';
 
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
+
+    // Auth Observer
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+            setLoading(false);
+        })
+
+        return () => unsubscribe();
+    }, [])
+
     const authInfo = {
         // AUthInfo here.
     }
