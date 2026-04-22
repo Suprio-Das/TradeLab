f0bf46ec82f2b641c3244da6e94ad5ed386e0529
Suprio Das <suprio.cse@gmail.com>
2026-04-22 12:08:29 +0600

    user and loading state created

--- frontend/src/Context/AuthProvider.jsx
+++ frontend/src/Context/AuthProvider.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AuthContext from './AuthContext';
 
 const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
     const authInfo = {
         // AUthInfo here.
     }
