2d44eaf7cbd8263f08809bbd35dbb5ddcd01d1e5
Suprio Das <suprio.cse@gmail.com>
2026-04-22 12:13:46 +0600

    signup handler created

--- frontend/src/Context/AuthProvider.jsx
+++ frontend/src/Context/AuthProvider.jsx
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import AuthContext from './AuthContext';
-import { onAuthStateChanged } from "firebase/auth";
+import { createUserWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
 import { auth } from '../../firebase.init';
 
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
+
+    const handleSignUpWithEmailAndPassword = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
 
     // Auth Observer
     useEffect(() => {
@@ -18,7 +22,7 @@ const AuthProvider = ({ children }) => {
     }, [])
 
     const authInfo = {
-        // AUthInfo here.
+        handleSignUpWithEmailAndPassword
     }
     return (
         <AuthContext.Provider value={authInfo}>
