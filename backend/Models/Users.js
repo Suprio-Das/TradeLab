eb15314265232d75ff0bce0ffb2dc71c3209f49d
Suprio Das <suprio.cse@gmail.com>
2026-04-23 20:16:35 +0600

    user model updated

--- backend/Models/Users.js
+++ backend/Models/Users.js
@@ -12,8 +12,12 @@ const UserSchema = new mongoose.Schema({
     balance: {
         type: Number,
         default: 10000
+    },
+    position: {
+        entryPrice: Number,
+        quantity: Number,
     }
-})
+}, { timestamps: true })
 
 const UserModel = mongoose.model('users', UserSchema);
 export default UserModel;
