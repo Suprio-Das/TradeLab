5fe0ba9d7e87e6c0764483a0d7af9277b9d94147
Suprio Das <suprio.cse@gmail.com>
2026-04-23 23:07:28 +0600

    handleSell function created

--- frontend/src/Components/Dashboard.jsx
+++ frontend/src/Components/Dashboard.jsx
@@ -55,6 +55,18 @@ const Dashboard = () => {
         }
     };
 
+    const handleSell = async () => {
+        try {
+            console.log("Handle Sell")
+        } catch (err) {
+            if (err.response) {
+                alert(err.response.data.message);
+            } else {
+                alert("Something went wrong");
+            }
+        }
+    }
+
     return (
         <div className="min-h-screen bg-gray-900 text-white p-4">
             <div className="grid grid-cols-1 md:grid-cols-4 gap-4 min-h-[90vh]">
@@ -93,7 +105,7 @@ const Dashboard = () => {
                             <button onClick={handleBuy} className="flex-1 border border-green-500 text-green-500 py-2 hover:bg-green-500 hover:text-black transition">
                                 BUY
                             </button>
-                            <button className="flex-1 border border-red-500 text-red-500 py-2 hover:bg-red-500 hover:text-black transition">
+                            <button onClick={handleSell} className="flex-1 border border-red-500 text-red-500 py-2 hover:bg-red-500 hover:text-black transition">
                                 SELL
                             </button>
                         </div>
