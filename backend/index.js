7f37d770f47e1b355fc1795556754a9bff75596b
Suprio Das <suprio.cse@gmail.com>
2026-04-23 11:45:21 +0600

    Basic node-express server is created and running

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:5173/'
+}))
+
+app.get("/", (req, res) => {
+    res.send("API is running...");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
