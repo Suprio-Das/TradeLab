166727154187357650c8b02f4d561368e6126a89
Suprio Das <suprio.cse@gmail.com>
2026-04-23 22:08:09 +0600

    user fetched on buy trade controller

--- backend/Controllers/TradeController.js
+++ backend/Controllers/TradeController.js
@@ -1,6 +1,19 @@
+import UserModel from "../Models/Users.js";
+
 export const BuyTrade = async (req, res) => {
     try {
-        console.log("Buy trades.")
+        try {
+            const { userId, price, quantity } = req.body;
+
+            const user = await UserModel.findOne({ userId: userId });
+
+            if (!user) {
+                return res.status(404).json({ message: "User not found" });
+            }
+
+        } catch (err) {
+            res.status(500).json({ error: err.message });
+        }
     } catch (error) {
         res.status(500).json({ error: err.message });
     }
