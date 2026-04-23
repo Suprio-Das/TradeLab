b6c77a34a603de44dc0f8cadc236a052e74ecdd6
Suprio Das <suprio.cse@gmail.com>
2026-04-23 22:30:12 +0600

    handleBuy function implemented

--- frontend/src/Components/Dashboard.jsx
+++ frontend/src/Components/Dashboard.jsx
@@ -23,6 +23,10 @@ const Dashboard = () => {
         return null;
     }
 
+    const handleBuy = async () => {
+        console.log("Buy function")
+    };
+
     return (
         <div className="min-h-screen bg-gray-900 text-white p-4">
             <div className="grid grid-cols-1 md:grid-cols-4 gap-4 min-h-[90vh]">
@@ -58,7 +62,7 @@ const Dashboard = () => {
                         />
 
                         <div className="flex gap-2">
-                            <button className="flex-1 border border-green-500 text-green-500 py-2 hover:bg-green-500 hover:text-black transition">
+                            <button onClick={handleBuy} className="flex-1 border border-green-500 text-green-500 py-2 hover:bg-green-500 hover:text-black transition">
                                 BUY
                             </button>
                             <button className="flex-1 border border-red-500 text-red-500 py-2 hover:bg-red-500 hover:text-black transition">
