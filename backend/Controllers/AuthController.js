b9ec48f163fc383d04d15459efec978d882e6ae0
Suprio Das <suprio.cse@gmail.com>
2026-04-23 13:11:37 +0600

    Signup controller implemented

--- backend/Controllers/AuthController.js
+++ backend/Controllers/AuthController.js
@@ -1,6 +1,17 @@
+import { model } from "mongoose";
+import UserModel from "../Models/Users.js";
+
 export const Signup = async (req, res) => {
     try {
-        console.log("Auth Controller.")
+        const { name, email } = req.body;
+
+        if (!name || !email) {
+            return res.status(404).json({ message: "Name or Email can not be empty" });
+        }
+
+        const user = await UserModel.insertOne({ name, email });
+
+        res.status(200).json({ success: true, message: 'User registered successful' });
     } catch (error) {
         console.log(error.message);
     }
